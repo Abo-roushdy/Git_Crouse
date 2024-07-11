4fd6fc46b7331ae40e5466cf5f10bafb617ef98f
Ahmed Roushdy <aroushdy115@gmail.com>
2024-07-11 20:20:49 +0300

    Delete task_flutter/clac_bloc/clac_bloc directory

--- task_flutter/clac_bloc/clac_bloc/android/app/src/main/kotlin/com/example/clac_bloc/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.clac_bloc
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- task_flutter/clac_bloc/clac_bloc/lib/bloc/counter_event.dart
+++
@@ -1,7 +0,0 @@
-part of 'counter_bloc.dart';
-
-@immutable
-sealed class CounterEvent {}
-class IncrementEvent extends CounterEvent{}
-class DEcrementEvent extends CounterEvent{}
-class ResttEvent extends CounterEvent{}
